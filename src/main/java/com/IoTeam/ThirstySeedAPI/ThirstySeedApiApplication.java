fc772c5524c0efc992aff53fb1b9afbf3356ce74
Shayla Choque <u20201c144@upc.edu.pe>
2024-10-22 18:34:56 -0500

    Initial commit

---
+++ src/main/java/com/IoTeam/ThirstySeedAPI/ThirstySeedApiApplication.java
@@ -0,0 +1,13 @@
+package com.IoTeam.ThirstySeedAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirstySeedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirstySeedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IoTeam/ThirstySeedAPI/ThirstySeedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IoTeam.ThirstySeedAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirstySeedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
