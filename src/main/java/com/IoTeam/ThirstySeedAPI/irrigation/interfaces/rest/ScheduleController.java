0c2f119dffd3c800e5e774797984b24642002360
Shayla Choque <u20201c144@upc.edu.pe>
2024-10-30 21:43:50 -0500

    fix: schedule aggregate y service

---
+++ src/main/java/com/IoTeam/ThirstySeedAPI/irrigation/interfaces/rest/ScheduleController.java
@@ -0,0 +1,87 @@
+package com.IoTeam.ThirstySeedAPI.irrigation.interfaces.rest;
+
+import com.IoTeam.ThirstySeedAPI.irrigation.application.internal.commandservices.ScheduleCommandServiceImpl;
+import com.IoTeam.ThirstySeedAPI.irrigation.application.internal.queryservices.ScheduleQueryServiceImpl;
+import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.commands.DeleteScheduleCommand;
+import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.queries.GetAllSchedulesQuery;
+import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.queries.GetScheduleByIdQuery;
+import com.IoTeam.ThirstySeedAPI.irrigation.interfaces.rest.resources.CreateScheduleResource;
+import com.IoTeam.ThirstySeedAPI.irrigation.interfaces.rest.resources.ScheduleResource;
+import com.IoTeam.ThirstySeedAPI.irrigation.interfaces.rest.transform.CreateScheduleCommandFromResourceAssembler;
+import com.IoTeam.ThirstySeedAPI.irrigation.interfaces.rest.transform.ScheduleResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/api/v1/schedules", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Schedules", description = "Schedule Management Endpoints")
+public class ScheduleController {
+
+    private final ScheduleCommandServiceImpl scheduleCommandServiceImpl;
+    private final ScheduleQueryServiceImpl scheduleQueryServiceImpl;
+
+    public ScheduleController(ScheduleCommandServiceImpl scheduleCommandServiceImpl, ScheduleQueryServiceImpl scheduleQueryServiceImpl) {
+        this.scheduleCommandServiceImpl = scheduleCommandServiceImpl;
+        this.scheduleQueryServiceImpl = scheduleQueryServiceImpl;
+    }
+
+    @PostMapping
+    public ResponseEntity<ScheduleResource> createSchedule(@RequestBody CreateScheduleResource resource) {
+        try {
+            var createScheduleCommand = CreateScheduleCommandFromResourceAssembler.toCommandFromResource(resource);
+            var scheduleId = scheduleCommandServiceImpl.handle(createScheduleCommand);
+            if (scheduleId == 0L) {
+                return ResponseEntity.badRequest().build();
+            }
+            var getScheduleByIdQuery = new GetScheduleByIdQuery(scheduleId);
+            var schedule = scheduleQueryServiceImpl.handle(getScheduleByIdQuery);
+
+            if (schedule.isEmpty()) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            var scheduleResource = ScheduleResourceFromEntityAssembler.toResourceFromEntity(schedule.get());
+            return new ResponseEntity<>(scheduleResource, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{scheduleId}")
+    public ResponseEntity<ScheduleResource> getScheduleById(@PathVariable Long scheduleId) {
+        var getScheduleByIdQuery = new GetScheduleByIdQuery(scheduleId);
+        var schedule = scheduleQueryServiceImpl.handle(getScheduleByIdQuery);
+        if (schedule.isEmpty()) return ResponseEntity.badRequest().build();
+        var scheduleResource = ScheduleResourceFromEntityAssembler.toResourceFromEntity(schedule.get());
+        return ResponseEntity.ok(scheduleResource);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScheduleResource>> getAllSchedules() {
+        var query = new GetAllSchedulesQuery();
+        var listSchedules = scheduleQueryServiceImpl.handle(query);
+        List<ScheduleResource> scheduleResources = listSchedules.stream()
+                .map(ScheduleResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(scheduleResources);
+    }
+
+    @DeleteMapping("/{scheduleId}")
+    public ResponseEntity<Void> deleteSchedule(@PathVariable Long scheduleId) {
+        try {
+            var deleteScheduleCommand = new DeleteScheduleCommand(scheduleId);
+            scheduleCommandServiceImpl.handle(deleteScheduleCommand);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
