a28c94b981bc90d2b742fbc6f31b54b338feee7b
RafaelLuyo <u201919295@upc.edu.pe>
2024-11-02 16:48:56 -0500

    security modified

--- src/main/java/com/IoTeam/ThirstySeedAPI/shared/configuration/SecurityConfig.java
+++ src/main/java/com/IoTeam/ThirstySeedAPI/shared/configuration/SecurityConfig.java
@@ -5,23 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        // Permitir acceso sin autenticación a Swagger y otros recursos públicos
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        // Proteger el resto de endpoints
                         .anyRequest().authenticated()
                 )
-                .httpBasic(customizer -> customizer.disable()) // Deshabilita la autenticación HTTP básica
-                .formLogin(customizer -> customizer.disable()) // Deshabilita la autenticación basada en formularios
+                .httpBasic(customizer -> customizer.disable())
+                .formLogin(customizer -> customizer.disable())
                 .csrf(csrf -> csrf.disable()); // Deshabilita la protección CSRF
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Permitir todos los orígenes
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+        configuration.setAllowedHeaders(List.of("*")); // Encabezados permitidos
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Permitir CORS en todas las rutas
+        return source;
     }
 }
