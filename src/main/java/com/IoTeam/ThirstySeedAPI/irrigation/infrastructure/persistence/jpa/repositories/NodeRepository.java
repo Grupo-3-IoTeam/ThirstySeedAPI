30bfacf25bc97eb2b2e31d2bc2f4caac7f48fe3d
RafaelLuyo <u201919295@upc.edu.pe>
2024-11-06 10:50:16 -0500

    feat(productcode): Node Repository modified

--- src/main/java/com/IoTeam/ThirstySeedAPI/irrigation/infrastructure/persistence/jpa/repositories/NodeRepository.java
+++ src/main/java/com/IoTeam/ThirstySeedAPI/irrigation/infrastructure/persistence/jpa/repositories/NodeRepository.java
@@ -4,8 +4,10 @@ import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.aggregates.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NodeRepository extends JpaRepository<Node,Long> {
 
     List<Node> findByPlotId(Long idPlot);
+    Optional<Node> findByProductCode(String code);
 }
