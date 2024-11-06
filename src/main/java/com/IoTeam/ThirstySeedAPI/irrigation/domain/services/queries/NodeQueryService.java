4a77863c3b8c45a0f6789245e8528cc8d651a8bf
RafaelLuyo <u201919295@upc.edu.pe>
2024-11-06 10:52:44 -0500

    feat(Âproductcode): Nodequery modified

--- src/main/java/com/IoTeam/ThirstySeedAPI/irrigation/domain/services/queries/NodeQueryService.java
+++ src/main/java/com/IoTeam/ThirstySeedAPI/irrigation/domain/services/queries/NodeQueryService.java
@@ -3,6 +3,7 @@ package com.IoTeam.ThirstySeedAPI.irrigation.domain.services.queries;
 import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.aggregates.Node;
 import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.queries.GetNodeByIdQuery;
 import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.queries.GetNodeByPlotIdQuery;
+import com.IoTeam.ThirstySeedAPI.irrigation.domain.model.queries.GetNodeByProductCodeQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,6 @@ import java.util.Optional;
 public interface NodeQueryService {
     Optional<Node> findNodeById(GetNodeByIdQuery query);
     Optional<Node> findNodeByPlotId(GetNodeByPlotIdQuery query);
+    Optional<Node> findNodeByProductCode(GetNodeByProductCodeQuery query);
     List<Node> findAllNodes();
 }
