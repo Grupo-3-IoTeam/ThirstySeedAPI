bfd00f2700c9b4eba4ec29837e9edf2c8439bc6e
Shayla Choque <u20201c144@upc.edu.pe>
2024-11-13 22:11:07 -0500

    feat: implement infrastructure layer

---
+++ src/main/java/com/IoTeam/ThirstySeedAPI/profile/infrastructure/persistence/jpa/repositories/ProfileRepository.java
@@ -0,0 +1,13 @@
+package com.IoTeam.ThirstySeedAPI.profile.infrastructure.persistence.jpa.repositories;
+
+import com.IoTeam.ThirstySeedAPI.profile.domain.model.aggregates.Profile;
+import com.IoTeam.ThirstySeedAPI.profile.domain.model.valueobjects.EmailAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByEmail(EmailAddress emailAddress);
+}
