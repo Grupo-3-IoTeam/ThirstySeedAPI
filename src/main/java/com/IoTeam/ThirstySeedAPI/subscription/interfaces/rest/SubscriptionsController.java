2d06c2c37701441c7d43bc38fddea8cabfc1f02e
Shayla Choque <u20201c144@upc.edu.pe>
2024-11-15 16:14:35 -0500

    fix: delete unused imports

--- src/main/java/com/IoTeam/ThirstySeedAPI/subscription/interfaces/rest/SubscriptionsController.java
+++ src/main/java/com/IoTeam/ThirstySeedAPI/subscription/interfaces/rest/SubscriptionsController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/com/IoTeam/ThirstySeedAPI/subscription/interfaces/rest/resources/CreateSubscriptionResource.java
+++ src/main/java/com/IoTeam/ThirstySeedAPI/subscription/interfaces/rest/resources/CreateSubscriptionResource.java
@@ -1,8 +1,6 @@
 package com.IoTeam.ThirstySeedAPI.subscription.interfaces.rest.resources;
 
 import com.IoTeam.ThirstySeedAPI.subscription.domain.model.valueobjects.PlanType;
-import com.IoTeam.ThirstySeedAPI.subscription.domain.model.valueobjects.NodeCount;
-import com.IoTeam.ThirstySeedAPI.subscription.domain.model.valueobjects.ValidationCode;
 
 public record CreateSubscriptionResource(
         Long userId,
